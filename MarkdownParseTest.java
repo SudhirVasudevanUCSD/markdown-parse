7468cb40df57d34aac5386ef60016b57d02937ab
SudhirVasudevanUCSD <97704519+SudhirVasudevanUCSD@users.noreply.github.com>
2022-01-27 17:47:35 -0800

    Test-file content

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,19 @@
+
 import static org.junit.Assert.*;
 
 import org.junit.*;
+
+import java.io.IndexOutOfBoundsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    public void testIt() throws IndexOutOfBoundsException {
+        assertEquals(List.of("https://something.com", "some-page.html")),
+                MarkdownParse.getLinks(Files.readString(Path.of(("test.md"))));
     }
 }
