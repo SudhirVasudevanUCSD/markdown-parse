e5f11691e4f03a95f71fb6375c996ab3c0781810
SudhirVasudevanUCSD <97704519+SudhirVasudevanUCSD@users.noreply.github.com>
2022-02-03 14:32:46 -0800

    update

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,4 @@
-
 import static org.junit.Assert.*;
-
 import org.junit.*;
 
 import java.io.IOException;
@@ -8,11 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-
 public class MarkdownParseTest {
     @Test
     public void testTestFile1() throws IOException {
-        System.out.println("test ran");
         assertEquals(new ArrayList<String>(),
                 MarkdownParse.getLinks(Files.readString(Path.of(("test-file" +
                         ".md")))));
